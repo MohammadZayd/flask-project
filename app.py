da8b7a343b9de5ac47220caeea18d00a2518cdc8
mohammadzayd <106404449+MohammadZayd@users.noreply.github.com>
2023-12-08 01:30:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
     return 'Hello  Building CI/CD with aws services '
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port='5000') 
+    app.run(host="0.0.0.0",port='5000')  
 
