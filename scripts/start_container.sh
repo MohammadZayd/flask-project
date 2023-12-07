4b701d71eae89e9a113ca07ec36c18faef29f762
mohammadzayd <106404449+MohammadZayd@users.noreply.github.com>
2023-12-07 18:45:55 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull zayd123/simple-python-flask-app:latest 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 zayd123/simple-python-flask-app:latest 
+docker run -d -p 5000:5000 zayd123/simple-python-flask-app:latest  
